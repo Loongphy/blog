5217805b0a13a06f4f5b4aff39dd6782592d1524
Loongphy <Loongphy@outlook.com>
2023-03-04 22:43:25 +0800

    Initial commit

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+export const SITE_TITLE = `天真的和伤感的梦想家`;
+export const SITE_DESCRIPTION = 'Austin Site Description';
+export const SITE_EMAIL = 'loongphy@outlook.com'
+export const SITE_NAME = '不见白衣';
+export const SITE_URL = "https://blog.loongphy.com";
