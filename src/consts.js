68a948300c5d9e74f7b5344e781f0fb47488edc8
Loongphy <Loongphy@outlook.com>
2023-03-18 15:17:36 +0800

    fix: replace rss url

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `天真的和伤感的梦想家`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
+export const SITE_DESCRIPTION = '懒狗的自我修养，还有一些个人分享';
 export const SITE_EMAIL = 'loongphy@outlook.com'
 export const SITE_NAME = '不见白衣';
 export const SITE_URL = "https://blog.loongphy.com";
