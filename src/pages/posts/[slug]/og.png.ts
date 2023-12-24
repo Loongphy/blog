c91479d586f21e32ee25bc3fe781c609ddac834b
Loongphy <Loongphy@outlook.com>
2023-12-24 20:25:42 +0800

    style(og): 调整 OG image 自动生成样式

--- src/pages/posts/[slug]/og.png.ts
+++ src/pages/posts/[slug]/og.png.ts
@@ -2,59 +2,72 @@ import { getCollection, type CollectionEntry } from 'astro:content';
 import fs from 'fs';
 import path from 'path';
 import { ImageResponse } from '@vercel/og';
- 
+
 interface Props {
   params: { slug: string };
   props: { post: CollectionEntry<'posts'> };
 }
- 
+
 export async function GET({ props }: Props) {
   const { post } = props;
- 
+
   // using custom font files
   const SmileySans = fs.readFileSync(path.resolve('./public/SmileySans-Oblique.ttf'));
- 
- 
+  const HarmonyOSSans = fs.readFileSync(path.resolve('./public/HarmonyOS_Sans_SC_Regular.ttf'));
+
+
   // Astro doesn't support tsx endpoints so usign React-element objects
   const html = {
     type: 'div',
     props: {
+      tw: 'w-full h-full flex flex-col items-center justify-center relative px-22',
+      style: {
+        backgroundImage: 'linear-gradient(45deg, rgb(236,72,153) 0%, rgb(239,68,68) 50%, rgb(234,179,8) 100%)',
+        letterSpacing: -2,
+        fontWeight: 700,
+        textAlign: 'left',
+        fontFamily: '鸿蒙',
+      },
       children: [
         {
           type: 'div',
           props: {
-            tw: 'pl-10 shrink flex',
+            tw: 'flex flex-col justify-start',
             children: [
               {
                 type: 'div',
                 props: {
-                  style: {
-                    fontSize: '60',
-                    fontFamily: '得意黑',
-                    backgroundImage: 'linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))',
-                    backgroundClip: 'text',
-                    '-webkit-background-clip': 'text',
-                    color: 'transparent',
-                  },
-                  children: post.data.title,
+                  tw: 'flex',
+                  children: [
+                    {
+                      type: 'div',
+                      props: {
+                        style: {
+                          fontSize: 60,
+                          backgroundClip: 'text',
+                          '-webkit-background-clip': 'text',
+                        },
+                        children: post.data.title,
+                      },
+                    },
+                  ],
                 },
               },
-            ],
-          },
-        },
-        {
-          type: 'div',
-          props: {
-            style: {
-              fontSize: 48,
-              fontFamily: '得意黑',
-              backgroundImage: 'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
-              backgroundClip: 'text',
-              '-webkit-background-clip': 'text',
-              color: 'transparent',
-            },
-            children: post.data.description,
-          },
+              {
+                type: 'div',
+                props: {
+                  tw: 'pt-4',
+                  style: {
+                    fontSize: 30,
+                    fontFamily: '得意黑',
+                    backgroundClip: 'text',
+                    '-webkit-background-clip': 'text',
+                  },
+                  children: post.data.description,
+                },
+              },
+            ]
+          }
         },
         {
           type: 'div',
@@ -93,37 +106,25 @@ export async function GET({ props }: Props) {
           },
         },
       ],
-      tw: 'w-full h-full flex items-center justify-center relative px-22',
-      style: {
-        display: 'flex',
-        height: '100%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
-        fontSize: 60,
-        letterSpacing: -2,
-        fontWeight: 700,
-        textAlign: 'center',
-        fontFamily: '得意黑',
-      },
     },
   };
- 
+
   return new ImageResponse(html, {
-    width: 1200,
-    height: 600,
     fonts: [
       {
         name: '得意黑',
         data: SmileySans.buffer,
         style: 'normal',
       },
+      {
+        name: '鸿蒙',
+        data: HarmonyOSSans.buffer,
+        style: 'normal',
+      },
     ],
   });
 }
- 
+
 // to generate an image for each blog posts in a collection
 export async function getStaticPaths() {
   const blogPosts = await getCollection('posts');
